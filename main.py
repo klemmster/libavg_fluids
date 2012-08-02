755739188ee3de80e722aa09d8f48bb8a6270462
Richard Klemm <Richy@coding-reality.de>
2012-08-03 00:37:41 +0200

    Add CUDA to CMake

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ g_Size = (512, 512)
 
 class ShadowDrawApp(AVGApp):
     def init(self):
-        g_Player.pluginPath = "plugin/Release/"
         g_Player.loadPlugin("fluidnode")
         self.fluidsInstance = fluidnode.FluidNode(parent=self._parentNode,
             size=g_Size)
